f12e561364415e921465486ed6f6f87d164508bb
Julian Fausto <julianfausto@hotmail.com.ar>
2023-06-14 09:50:56 -0300

    Implement Player Movement

---
+++ Grupo_13_TP2/Assets/_Main/Scripts/ScriptableObjects/PlayerData.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace _Main.Scripts.ScriptableObjects
+{
+    [CreateAssetMenu(menuName = "Main/Player/PlayerData")]
+    public class PlayerData : ScriptableObject
+    {
+        [field: SerializeField] public float MovementSpeed { get; private set; } = 2;
+        [field: SerializeField] public float JumpForce { get; private set; } = 5;
+        [field: SerializeField] public float CheckGroundRadius { get; private set; } = 0.5f;
+        [field: SerializeField] public LayerMask CheckGroundLayerMask { get; private set; }
+    }
+}
